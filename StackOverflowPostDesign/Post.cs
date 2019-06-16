9e8b4c567fc490d371a25eb69e8b5cb28dd965dc
MayarAlaa <mayaralaa12@gmail.com>
2019-06-16 21:34:15 +0200

    Add files via upload

---
+++ StackOverflowPostDesign/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPostExercise
+{
+    public class Post
+    {
+        private int _vote ;
+
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+
+       
+        
+
+        public void UpVote()
+        {
+            _vote = _vote + 1;
+            Console.WriteLine("You've up-voted this post");
+
+        }
+
+        public void DownVote()
+        {
+            _vote = _vote - 1;
+            Console.WriteLine("You've down-voted this post");
+        }
+
+        public void DisplayVotesNumber()
+        {
+            Console.WriteLine($"The current vote value is {_vote}");
+        }
+    }
+}
---
+++ StackOverflowPostDesign/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPostExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post()
+            {
+                Title = "What's an interface?",
+                Description = "Explains the usage of interfaces.",
+                CreationDate = DateTime.Now
+
+            };
+
+            post.UpVote();
+
+            post.UpVote();
+
+            post.UpVote();
+
+            post.DisplayVotesNumber();
+
+            post.DownVote();
+
+            post.UpVote();
+
+            post.DownVote();
+
+            post.DownVote();
+
+            post.DisplayVotesNumber();
+
+
+
+        }
+    }
+}
