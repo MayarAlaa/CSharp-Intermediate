d2d2e08636530014a30a38cbf5bfd76436178d1c
MayarAlaa <mayaralaa12@gmail.com>
2019-06-16 21:21:03 +0200

    Add files via upload

---
+++ StopWatchDesign/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                var startTime = StopWatch.Start();
+
+                System.Console.WriteLine("Start Time : " + startTime);
+
+               Thread.Sleep(60000);
+                var endTime = StopWatch.Stop();
+
+                System.Console.WriteLine("End Time : " + endTime);
+
+                
+            
+            
+        }
+    }
+}
---
+++ StopWatchDesign/StopWatch.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public static class StopWatch
+    {
+        private static bool _isStopClicked = true ;
+        private static TimeSpan _startTime;
+        private static TimeSpan _endTime;
+
+
+        public static TimeSpan Start()
+        {
+            
+                if (_isStopClicked == false)
+                {
+                    throw new InvalidOperationException("The stopwatch is still running!!");
+
+                }
+
+                _isStopClicked = false;
+                _startTime = DateTime.Now.TimeOfDay;
+                return _startTime;
+
+            
+            
+           
+        }
+
+        public static TimeSpan Stop()
+        {
+            if (_isStopClicked == true)
+            {
+                throw new InvalidOperationException("The stopwatch isn't working!");
+            }
+            _isStopClicked = true;
+            _endTime = DateTime.Now.TimeOfDay;
+            return _endTime;
+
+        }
+
+
+        public static TimeSpan Duration()
+        {
+            return (_endTime - _startTime);
+
+        }
+
+
+    }
+}
